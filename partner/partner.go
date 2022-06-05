44e6a98d2367538a3cc496d30ea86db4cf03f36b
Andrys Daniel Silalahi <andrys.silalahi@grabtaxi.com>
2022-06-05 15:48:01 +0700

    partner package

---
+++ partner/partner.go
@@ -0,0 +1,117 @@
+package partner
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/andrysds/colly-example/product"
+)
+
+const (
+	usernameEnvKey          = "USERNAME"
+	passwordEnvKey          = "PASSWORD"
+	loginUrlEnvKey          = "LOGIN_URL"
+	getProductBaseUrlEnvKey = "GET_PRODUCT_BASE_URL"
+)
+
+type httpClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+type Partner struct {
+	httpClient        httpClient
+	authToken         string
+	username          string
+	password          string
+	loginUrl          string
+	getProductBaseUrl string
+}
+
+func NewPartner() *Partner {
+	return &Partner{
+		httpClient:        &http.Client{},
+		username:          os.Getenv(usernameEnvKey),
+		password:          os.Getenv(passwordEnvKey),
+		loginUrl:          os.Getenv(loginUrlEnvKey),
+		getProductBaseUrl: os.Getenv(getProductBaseUrlEnvKey),
+	}
+}
+
+type loginResponse struct {
+	Data struct {
+		Token string `json:"token"`
+	} `json:"data"`
+}
+
+func (p *Partner) Login() error {
+	jsonBody, err := json.Marshal(
+		map[string]string{
+			"username": p.username,
+			"password": p.password,
+		},
+	)
+	if err != nil {
+		return err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+
+	req, err := http.NewRequest(http.MethodPost, p.loginUrl, reqBody)
+	if err != nil {
+		return err
+	}
+
+	res, err := p.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		return fmt.Errorf("got this status code: %d", res.StatusCode)
+	}
+
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	var loginRes *loginResponse
+	err = json.Unmarshal(resBody, &loginRes)
+	if err != nil {
+		return err
+	}
+
+	p.authToken = loginRes.Data.Token
+	return nil
+}
+
+func (p *Partner) GetProduct(slug string) (*product.Product, error) {
+	req, err := http.NewRequest(http.MethodGet, p.getProductBaseUrl+slug, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", p.authToken)
+
+	res, err := p.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		return nil, fmt.Errorf("got this status code: %d", res.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var pp *product.Product
+	err = json.Unmarshal(body, &pp)
+	return pp, err
+}
---
+++ partner/partner_test.go
@@ -0,0 +1,221 @@
+package partner
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/andrysds/colly-example/product"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestNewPartner(t *testing.T) {
+	want := &Partner{
+		httpClient: &http.Client{},
+		authToken:  "",
+	}
+	if got := NewPartner(); !reflect.DeepEqual(got, want) {
+		t.Errorf("NewPartner() = %v, want %v", got, want)
+	}
+}
+
+func TestPartner_Login(t *testing.T) {
+	mockUsername := "sample username"
+	mockPassword := "sample password"
+	mockAuthToken := "sample auth token"
+	mockLoginURL := "https://example.com/login"
+
+	mockReqMatcher := func(req *http.Request) bool {
+		mockReqJsonBody, _ := json.Marshal(
+			map[string]string{
+				"username": mockUsername,
+				"password": mockPassword,
+			},
+		)
+		mockReqBody := bytes.NewBuffer(mockReqJsonBody)
+		mockReq, _ := http.NewRequest(http.MethodPost, mockLoginURL, mockReqBody)
+
+		return req.Method == mockReq.Method &&
+			reflect.DeepEqual(req.URL, mockReq.URL) &&
+			reflect.DeepEqual(req.Body, mockReq.Body)
+	}
+
+	tests := []struct {
+		name          string
+		httpClient    func() httpClient
+		wantErr       bool
+		wantAuthToken string
+	}{
+		{
+			name: "got error from httpClient",
+			httpClient: func() httpClient {
+				c := &mockHttpClient{}
+				c.On("Do", mock.MatchedBy(mockReqMatcher)).Return(nil, fmt.Errorf("sample error"))
+				return c
+			},
+			wantErr:       true,
+			wantAuthToken: "",
+		},
+		{
+			name: "got non 2xx response from httpClient",
+			httpClient: func() httpClient {
+				c := &mockHttpClient{}
+				mockRes := &http.Response{StatusCode: http.StatusBadRequest}
+				c.On("Do", mock.MatchedBy(mockReqMatcher)).Return(mockRes, nil)
+				return c
+			},
+			wantErr:       true,
+			wantAuthToken: "",
+		},
+		{
+			name: "happy path",
+			httpClient: func() httpClient {
+				c := &mockHttpClient{}
+
+				mockResJsonBody, _ := json.Marshal(
+					map[string]map[string]string{
+						"data": {
+							"token": mockAuthToken,
+						},
+					},
+				)
+				mockResBody := bytes.NewBuffer(mockResJsonBody)
+				mockRes := &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(mockResBody),
+				}
+
+				c.On("Do", mock.MatchedBy(mockReqMatcher)).Return(mockRes, nil)
+				return c
+			},
+			wantErr:       false,
+			wantAuthToken: mockAuthToken,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Partner{
+				httpClient: tt.httpClient(),
+				username:   mockUsername,
+				password:   mockPassword,
+				loginUrl:   mockLoginURL,
+			}
+			err := p.Login()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Partner.Login() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if p.authToken != tt.wantAuthToken {
+				t.Errorf("Partner.Login() Partner.auth = %v, wantErr %v", p.authToken, tt.wantAuthToken)
+			}
+		})
+	}
+}
+
+func TestPartner_GetProduct(t *testing.T) {
+	mockAuthToken := "sample auth token"
+	mockGetProductBaseUrl := "https://example.com/product/"
+	mockSlug := "sample-slug"
+
+	mockReqMatcher := func(req *http.Request) bool {
+		mockReq, _ := http.NewRequest(http.MethodGet, mockGetProductBaseUrl+mockSlug, nil)
+		mockReq.Header.Set("Authorization", mockAuthToken)
+		return req.Method == mockReq.Method &&
+			reflect.DeepEqual(req.URL, mockReq.URL) &&
+			reflect.DeepEqual(req.Header, mockReq.Header)
+	}
+
+	mockProduct := &product.Product{
+		Name:        "sample name",
+		Description: "sample description",
+		Variants: []product.Variant{
+			{
+				Name:  "sample name",
+				Price: 1000,
+				Stock: 0,
+			},
+		},
+	}
+
+	tests := []struct {
+		name       string
+		httpClient func() httpClient
+		want       *product.Product
+		wantErr    bool
+	}{
+		{
+			name: "got error from httpClient",
+			httpClient: func() httpClient {
+				c := &mockHttpClient{}
+				c.On("Do", mock.MatchedBy(mockReqMatcher)).Return(nil, fmt.Errorf("sample error"))
+				return c
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "got non 2xx response from httpClient",
+			httpClient: func() httpClient {
+				c := &mockHttpClient{}
+				mockRes := &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(bytes.NewBuffer([]byte(""))),
+				}
+				c.On("Do", mock.MatchedBy(mockReqMatcher)).Return(mockRes, nil)
+				return c
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "happy path",
+			httpClient: func() httpClient {
+				c := &mockHttpClient{}
+
+				mockResJsonBody, _ := json.Marshal(
+					map[string]interface{}{
+						"name":        mockProduct.Name,
+						"description": mockProduct.Description,
+						"variants": []map[string]interface{}{
+							{
+								"variants_name": mockProduct.Variants[0].Name,
+								"price":         mockProduct.Variants[0].Price,
+								"stock":         mockProduct.Variants[0].Stock,
+							},
+						},
+					},
+				)
+				mockResBody := bytes.NewBuffer(mockResJsonBody)
+				mockRes := &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(mockResBody),
+				}
+
+				c.On("Do", mock.MatchedBy(mockReqMatcher)).Return(mockRes, nil)
+				return c
+			},
+			want:    mockProduct,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Partner{
+				httpClient:        tt.httpClient(),
+				authToken:         mockAuthToken,
+				getProductBaseUrl: mockGetProductBaseUrl,
+			}
+			got, err := p.GetProduct(mockSlug)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Partner.GetProduct() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Partner.GetProduct() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
